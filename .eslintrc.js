4fee383b678ffa95e4f6e46baba9c4a23e11d94b
azusa5526 <azusa5526@gmail.com>
2020-08-22 16:53:07 +0800

    Revert "revert"
    This reverts commit c5c8131f0cd002a7ef1ebdd379913874336c5bfd.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/standard'],
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
   parserOptions: {
     parser: 'babel-eslint'
   },
@@ -12,8 +15,10 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     semi: [2, 'always'],
     'no-extra-semi': 0,
-    'semi-spacing': ['error', { before: false, after: true }],
-    'space-before-function-paren': 0,
-    'comma-dangle': ['error', 'never']
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'always',
+      asyncArrow: 'always'
+    }]
   }
 };
